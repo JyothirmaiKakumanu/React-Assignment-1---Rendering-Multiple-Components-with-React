8837521de89f165a1474d8be96636c0d97d227e6
JyothirmaiKakumanu <76103448+JyothirmaiKakumanu@users.noreply.github.com>
2021-02-12 11:57:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,13 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <label class="projectName">Project Name</label>
+      <p data-ns-attribute="project-name">Bomberman2</p>
+    <br>
+      <label class="projectDescription">Project Description</label>
+      <p data-ns-attribute="project-description">It looks interesting to code a game which we played in the childhood.</p>
+    </div>
   )
 }
 
