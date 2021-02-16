d6c03d1c0cc803a2656502e2d5f6ee43eaf39d90
JyothirmaiKakumanu <76103448+JyothirmaiKakumanu@users.noreply.github.com>
2021-02-16 22:49:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,6 @@ const App = () => {
     <div id="root" data-ns-test="">
       
       <h1 data-ns-test="project-name">Hangman</h1>
-    <br>
       <p data-ns-test="project-description">Takes input of random string and checks if character is present in the string. If hangman is completely hanged the game ends.</p>
     </div>
   )
